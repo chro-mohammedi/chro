25b6fd294c5f1ef14ad5d77d2e4d72429af7a965
chro-mohammedi <chro252868@spu.edu.iq>
2021-01-14 21:51:34 +0300

    test

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter your age please:");        
+            var ad= 2021;
+            var  year= Convert.ToInt32(Console.ReadLine());
+            var age =ad - year;
+            Console.WriteLine(" you are born on " + age);
+            Console.ReadKey();
+        }
+    }
+}
